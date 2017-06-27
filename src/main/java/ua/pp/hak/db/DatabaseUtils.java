589b325a12037d69a1de55e1ca4dcb58e4eab3b5
Valerii R <vr@gmail.com>
2017-06-28 02:02:09 +0300

    add support for multiple db

--- src/main/java/ua/pp/hak/db/DatabaseUtils.java
+++ src/main/java/ua/pp/hak/db/DatabaseUtils.java
@@ -1,5 +1,7 @@
 package ua.pp.hak.db;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -18,17 +20,62 @@ import ua.pp.hak.util.EncryptUtils;
 
 public class DatabaseUtils {
 	final static Logger logger = LogManager.getLogger(DatabaseUtils.class);
+	private final static String dbHashURL = "http://tpad.hak.pp.ua/db-hash.html";
 	public final static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
-	private static Connection getConnection() throws SQLException {
+	private static Connection getConnection(String hashUrl) throws SQLException {
 		String key = "tpad-pg";
-		String hashUrl = "HhQDBxcACAcEBhZIAxYYSk5LSBNVWUVRSRxJSkZBWUkcRldaEw4JXQUTEV1QSkwdBg4fDwVaA0kXHwxeGERURl8FBxpDURoGDlxYAQARRl4RXhUVSRUNAl4VFREaAA9fBRYGVhEFXgMQGwIFWR4SAUBHUlAVQ18WQ1JTTEMDEhYEABpAXhdIA1EcQVQQFVJSHEAGEEMHBkxIUBdIWQEYQARHQAMGTElfEElYARRWFAccDAtJFVoGFRARRAIC";
 		String dbUrl = EncryptUtils.xorMessage(EncryptUtils.base64decode(hashUrl), key);
 
 		return DriverManager.getConnection(dbUrl);
 	}
 
+	private static String getData(String address) throws Exception {
+		URL url = new URL(address);
+
+		InputStream html = null;
+
+		html = url.openStream();
+
+		int c = 0;
+		StringBuffer buffer = new StringBuffer("");
+
+		while (c != -1) {
+			c = html.read();
+
+			buffer.append((char) c);
+		}
+		return buffer.toString();
+	}
+
 	public List<Attribute> downloadAttributes() {
+		List<Attribute> attributes = new ArrayList<>();
+		String data = null;
+		try {
+			data = getData(dbHashURL);
+		} catch (Exception e) {
+			logger.error(e);
+		}
+		if (data == null) {
+			return attributes;
+		}
+
+		try {
+
+			final String hashTpadWeb = data.substring(data.indexOf("[tpadweb]") + 9, data.indexOf("[/tpadweb]"));
+			final String hashTpadWebDb = data.substring(data.indexOf("[tpadwebdb]") + 11, data.indexOf("[/tpadwebdb]"));
+
+			attributes.addAll(downloadAttributes(hashTpadWeb));
+			attributes.addAll(downloadAttributes(hashTpadWebDb));
+
+		} catch (Exception e) {
+			logger.error(e);
+		}
+
+		return attributes;
+	}
+
+	public List<Attribute> downloadAttributes(String hashUrl) {
 		List<Attribute> attributes = new ArrayList<>();
 		Connection con = null;
 		Statement stmt = null;
@@ -37,7 +84,7 @@ public class DatabaseUtils {
 		try {
 			Class.forName("org.postgresql.Driver");
 
-			con = getConnection();
+			con = getConnection(hashUrl);
 
 			stmt = con.createStatement();
 			rs = stmt.executeQuery("SELECT * FROM attributes");
@@ -81,6 +128,37 @@ public class DatabaseUtils {
 	}
 
 	public Timestamp downloadLastUpdate() {
+		String data = null;
+		try {
+			data = getData(dbHashURL);
+		} catch (Exception e) {
+			logger.error(e);
+		}
+		if (data == null) {
+			return null;
+		}
+
+		Timestamp ts1 = null;
+		Timestamp ts2 = null;
+		try {
+			final String hashTpadWeb = data.substring(data.indexOf("[tpadweb]") + 9, data.indexOf("[/tpadweb]"));
+			final String hashTpadWebDb = data.substring(data.indexOf("[tpadwebdb]") + 11, data.indexOf("[/tpadwebdb]"));
+
+			ts1 = downloadLastUpdate(hashTpadWeb);
+			ts2 = downloadLastUpdate(hashTpadWebDb);
+		} catch (Exception e) {
+			logger.error(e);
+		}
+
+		if (ts1 != null && ts2 != null) {
+			return ts1.before(ts2) ? ts2 : ts1;
+		}
+
+		return null;
+
+	}
+
+	public Timestamp downloadLastUpdate(String hashUrl) {
 		Connection con = null;
 		Statement stmt = null;
 		ResultSet rs = null;
@@ -89,7 +167,7 @@ public class DatabaseUtils {
 		try {
 			Class.forName("org.postgresql.Driver");
 
-			con = getConnection();
+			con = getConnection(hashUrl);
 
 			stmt = con.createStatement();
 			rs = stmt.executeQuery("SELECT MAX(updated) as max FROM update_log where table_name='attributes'");
