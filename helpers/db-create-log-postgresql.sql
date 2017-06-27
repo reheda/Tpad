c5c67d13ed63a85b530075205521d49dbdacc72d
Valerii R <vr@gmail.com>
2017-06-28 01:03:00 +0300

    add UPSERT to Update_log table

--- helpers/db-create-log-postgresql.sql
+++ helpers/db-create-log-postgresql.sql
@@ -1,8 +1,10 @@
 -- https://stackoverflow.com/questions/899203/how-do-i-find-the-last-time-that-a-postgresql-database-has-been-updated
-CREATE TABLE update_log(table_name text, updated timestamp NOT NULL DEFAULT now());
+CREATE TABLE update_log(table_name text PRIMARY KEY, updated timestamp NOT NULL DEFAULT now());
 CREATE FUNCTION stamp_update_log() RETURNS TRIGGER LANGUAGE 'plpgsql' AS $$
 BEGIN
-  INSERT INTO update_log(table_name) VALUES(TG_TABLE_NAME);
+  INSERT INTO update_log(table_name) VALUES(TG_TABLE_NAME)
+  ON CONFLICT (table_name) DO UPDATE 
+  	SET updated = DEFAULT;
   RETURN NEW;
 END
 $$;
